65b2fb74365eebd39e757a409cf997d9457963ff
AnyKnown <156967900+Anyknown@users.noreply.github.com>
2024-01-24 09:29:32 -0500

    Update stock_fly.py

--- stock_fly.py
+++ stock_fly.py
@@ -1,7 +1,6 @@
 import os
 import sys
 
-
 if not os.path.exists(".yfinance-installed"):
     print("Installing yfinance...")
     os.system("pip install yfinance")
@@ -10,34 +9,24 @@ if not os.path.exists(".yfinance-installed"):
 import yfinance as yf
 import pandas as pd
 
-
 percent_change = 0
-
 tickers = sys.argv[1:]
 
-
 data = pd.DataFrame()
-
 
 data["Purchase Price"] = []
 data["Current Price"] = []
 data["% Change"] = []
 
-
-for ticker in tickers:
-    stock = yf.Ticker(ticker)
-    current_price = stock.price
-    data.loc[data["Current Price"] == "", "Current Price"] = current_price
-
+stock = yf.Ticker(ticker)
+current_price = stock.price
+data.loc[data["Current Price"] == "", "Current Price"] = current_price
 
 for index, row in data.iterrows():
-    
-if row["Purchase Price"] != 0:
+    if row["Purchase Price"] != 0:
         percent_change = (current_price - row["Purchase Price"]) / row["Purchase Price"]
     else:
         percent_change = 0
-
 data.loc[index, "% Change"] = percent_change
-
 
 print(data)
