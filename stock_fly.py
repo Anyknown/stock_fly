c19fa7ac17f509c1aaf92ee38b20ed327d9f6db6
AnyKnown <156967900+Anyknown@users.noreply.github.com>
2024-01-24 13:56:43 -0500

    Update stock_fly.py

--- stock_fly.py
+++ stock_fly.py
@@ -13,25 +13,31 @@ if not os.path.exists(".yfinance-installed"):
 tickers = ["DOCN", "U", "TSLA", "AMZN"]
 purchase_price = [None, None, None, None]
 
+
 data = pd.DataFrame()
+
 
 data["Purchase Price"] = purchase_price
 data["Current Price"] = np.empty(len(tickers))
 data["% Change"] = np.empty(len(tickers))
 
+
 for ticker, purchase_price in zip(tickers, purchase_price):
-    stock = yf.Ticker(ticker)
+    
+stock = yf.Ticker(ticker)
     stock_info = stock.info
 
-    current_price = stock_info["price"]
-
+    
+current_price = stock_info["regularMarketPrice"]
     if purchase_price is None:
         percent_change = 0
 else:
         percent_change = (current_price - purchase_price) / purchase_price
 
+    
         data.loc[data["Current Price"] == "", "Current Price"] = current_price
         data.loc[data["Purchase Price"] == "", "Purchase Price"] = purchase_price
         data.loc[data["% Change"] == "", "% Change"] = percent_change
+
 
 print(data)
