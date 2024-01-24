ef1e03c9761a0a554f05577cd7bf488bf5bb0224
AnyKnown <156967900+Anyknown@users.noreply.github.com>
2024-01-24 13:58:51 -0500

    Update stock_fly.py

--- stock_fly.py
+++ stock_fly.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import os
 import sys
 import requests
@@ -10,34 +11,27 @@ if not os.path.exists(".yfinance-installed"):
     os.system("pip install yfinance")
     open(".yfinance-installed", "w").close()
 
-tickers = ["DOCN", "U", "TSLA", "AMZN"]
+tickers = ["AAPL", "GOOGL", "MSFT", "AMZN"]
 purchase_price = [None, None, None, None]
 
-
 data = pd.DataFrame()
-
 
 data["Purchase Price"] = purchase_price
 data["Current Price"] = np.empty(len(tickers))
 data["% Change"] = np.empty(len(tickers))
 
-
 for ticker, purchase_price in zip(tickers, purchase_price):
-    
-stock = yf.Ticker(ticker)
+    stock = yf.Ticker(ticker)
     stock_info = stock.info
 
-    
-current_price = stock_info["regularMarketPrice"]
+    current_price = stock_info["regularMarketPrice"]
     if purchase_price is None:
         percent_change = 0
 else:
         percent_change = (current_price - purchase_price) / purchase_price
 
-    
-        data.loc[data["Current Price"] == "", "Current Price"] = current_price
-        data.loc[data["Purchase Price"] == "", "Purchase Price"] = purchase_price
-        data.loc[data["% Change"] == "", "% Change"] = percent_change
-
+    data.loc[data["Current Price"] == "", "Current Price"] = current_price
+    data.loc[data["Purchase Price"] == "", "Purchase Price"] = purchase_price
+    data.loc[data["% Change"] == "", "% Change"] = percent_change
 
 print(data)
