cd44f8a63e5cf46da459c68171f7788076cec40c
AnyKnown <156967900+Anyknown@users.noreply.github.com>
2024-01-24 08:57:49 -0500

    Update stock_fly.py

--- stock_fly.py
+++ stock_fly.py
@@ -2,18 +2,30 @@ import yahoo_fin
 import pandas as pd
 import sys
 
+
 tickers = ["NFLX", "U", "TSLA", "DOCN"]
+
 
 purchase_prices = sys.argv[1:]
 
+
+purchase_prices = [float(price) for price in purchase_prices]
+
+
 data = pd.DataFrame()
+
 
 data["Purchase Price"] = []
 data["Current Price"] = []
 data["% Change"] = []
 
+
 for index, ticker in enumerate(tickers):
-    data.loc[index, "Purchase Price"] = float(purchase_prices[index])
+  
+if index < len(purchase_prices):
+        data.loc[index, "Purchase Price"] = purchase_prices[index]
+    else:
+        data.loc[index, "Purchase Price"] = 0
 
 for ticker in tickers:
     stock = yahoo_fin.StockInfo(ticker)
@@ -22,7 +34,10 @@ for ticker in tickers:
 
 for index, row in data.iterrows():
     current_price = data.loc[index, "Current Price"]
-    percent_change = (current_price - row["Purchase Price"]) / row["Purchase Price"]
-    data.loc[index, "% Change"] = percent_change
+    if row["Purchase Price"] != 0:
+        percent_change = (current_price - row["Purchase Price"]) / row["Purchase Price"]
+    else:
+        percent_change = 0
+data.loc[index, "% Change"] = percent_change
 
 print(data)
